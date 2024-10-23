80a1ba7f20bc457e4941343fadb4d1b80bb6723c
Erick Osuna Garcia <127696393+Erickzzxc@users.noreply.github.com>
2024-10-23 01:17:37 -0700

    Add project files.

---
+++ Pokedex/Form1.Designer.cs
@@ -0,0 +1,257 @@
+﻿namespace Pokedex
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lb2 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.labelpokemon = new System.Windows.Forms.Label();
+            this.lblID = new System.Windows.Forms.Label();
+            this.lstTipos = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pbSprite = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbSprite)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lb2
+            // 
+            this.lb2.AutoSize = true;
+            this.lb2.BackColor = System.Drawing.Color.Black;
+            this.lb2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.lb2.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.lb2.Location = new System.Drawing.Point(388, 390);
+            this.lb2.Name = "lb2";
+            this.lb2.Size = new System.Drawing.Size(19, 16);
+            this.lb2.TabIndex = 3;
+            this.lb2.Text = "....";
+            this.lb2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lb2.Click += new System.EventHandler(this.lb2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Black;
+            this.label2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(244, 416);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(112, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Numero Pokedex";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Black;
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(244, 442);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(42, 16);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Tipos";
+            // 
+            // labelpokemon
+            // 
+            this.labelpokemon.AutoSize = true;
+            this.labelpokemon.BackColor = System.Drawing.Color.Black;
+            this.labelpokemon.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.labelpokemon.ForeColor = System.Drawing.Color.White;
+            this.labelpokemon.Location = new System.Drawing.Point(244, 390);
+            this.labelpokemon.Name = "labelpokemon";
+            this.labelpokemon.Size = new System.Drawing.Size(117, 16);
+            this.labelpokemon.TabIndex = 7;
+            this.labelpokemon.Text = "Nombre Pokemon";
+            // 
+            // lblID
+            // 
+            this.lblID.AutoSize = true;
+            this.lblID.BackColor = System.Drawing.Color.Black;
+            this.lblID.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.lblID.ForeColor = System.Drawing.Color.White;
+            this.lblID.Location = new System.Drawing.Point(388, 416);
+            this.lblID.Name = "lblID";
+            this.lblID.Size = new System.Drawing.Size(19, 16);
+            this.lblID.TabIndex = 8;
+            this.lblID.Text = "....";
+            // 
+            // lstTipos
+            // 
+            this.lstTipos.BackColor = System.Drawing.Color.Black;
+            this.lstTipos.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.lstTipos.ForeColor = System.Drawing.SystemColors.Info;
+            this.lstTipos.FormattingEnabled = true;
+            this.lstTipos.ItemHeight = 16;
+            this.lstTipos.Location = new System.Drawing.Point(391, 442);
+            this.lstTipos.Name = "lstTipos";
+            this.lstTipos.Size = new System.Drawing.Size(85, 32);
+            this.lstTipos.TabIndex = 11;
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Yellow;
+            this.label1.Location = new System.Drawing.Point(230, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(387, 53);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Pokedex";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(10)))), ((int)(((byte)(43)))));
+            this.textBox1.Font = new System.Drawing.Font("Microsoft YaHei", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.ForeColor = System.Drawing.Color.Gold;
+            this.textBox1.Location = new System.Drawing.Point(257, 499);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(244, 56);
+            this.textBox1.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Gold;
+            this.label4.ForeColor = System.Drawing.Color.Black;
+            this.label4.Location = new System.Drawing.Point(341, 256);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(103, 16);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Sprite Pokemon";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Transparent;
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.lstTipos);
+            this.panel1.Controls.Add(this.lblID);
+            this.panel1.Controls.Add(this.lb2);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.labelpokemon);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.pbSprite);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Location = new System.Drawing.Point(25, 40);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(817, 625);
+            this.panel1.TabIndex = 13;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Black;
+            this.button1.BackgroundImage = global::Pokedex.Properties.Resources.ss_boton_removebg_preview;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button1.Location = new System.Drawing.Point(598, 416);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(117, 113);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "X";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pbSprite
+            // 
+            this.pbSprite.BackColor = System.Drawing.Color.Transparent;
+            this.pbSprite.BackgroundImage = global::Pokedex.Properties.Resources.fondoPokemon;
+            this.pbSprite.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pbSprite.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pbSprite.Location = new System.Drawing.Point(257, 68);
+            this.pbSprite.Name = "pbSprite";
+            this.pbSprite.Size = new System.Drawing.Size(280, 204);
+            this.pbSprite.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbSprite.TabIndex = 12;
+            this.pbSprite.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Black;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.pictureBox1.Image = global::Pokedex.Properties.Resources.PokedexSinnoh;
+            this.pictureBox1.Location = new System.Drawing.Point(3, -3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(809, 637);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(849, 686);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Pokedex Nacional";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbSprite)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lb2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label labelpokemon;
+        private System.Windows.Forms.Label lblID;
+        private System.Windows.Forms.ListBox lstTipos;
+        private System.Windows.Forms.PictureBox pbSprite;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ Pokedex/Form1.cs
@@ -0,0 +1,182 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pokedex
+{
+    public partial class Form1 : Form
+    {
+
+
+        private static readonly HttpClient client = new HttpClient(); // Reutilizamos el cliente
+
+
+        public Form1()
+        {
+            
+            InitializeComponent();
+            CargarPokemonAsync("pikachu");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+
+
+        public class PokemonData
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public SpriteData Sprites { get; set; }
+            public List<TypeData> Types { get; set; }
+        }
+
+        public class TypeData
+        {
+            public TypeDetail Type { get; set; }
+        }
+
+        public class SpriteData
+        {
+            [JsonProperty("front_default")] // Indicar el nombre de la propiedad en el JSON
+            public string FrontDefault { get; set; }
+        }
+
+
+
+        public class TypeDetail
+        {
+            public string Name { get; set; }
+        }
+
+
+        private async Task CargarPokemonAsync(string nombrePokemon)
+        {
+            string apiUrl = $"https://pokeapi.co/api/v2/pokemon/{nombrePokemon.ToLower()}";
+
+            try
+            {
+                HttpResponseMessage response = await client.GetAsync(apiUrl);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string jsonContent = await response.Content.ReadAsStringAsync();
+                    PokemonData pokemon = JsonConvert.DeserializeObject<PokemonData>(jsonContent);
+
+                    // Mostrar datos en consola
+                    Console.WriteLine($"Nombre: {pokemon.Name}");
+                    Console.WriteLine($"ID: {pokemon.Id}");
+                    Console.WriteLine($"Sprite: {pokemon.Sprites.FrontDefault}");
+
+                    Console.WriteLine("Tipos:");
+                    foreach (var tipo in pokemon.Types)
+                    {
+                        Console.WriteLine($"- {tipo.Type.Name}");
+                    }
+
+                    // Asignar datos a los controles de la UI
+                    lb2.Text = $"Nombre: {pokemon.Name}";
+                    lblID.Text = $"ID: {pokemon.Id}";
+
+                    lstTipos.Items.Clear(); // Limpiar la lista de tipos
+                    foreach (var tipo in pokemon.Types)
+                    {
+                        lstTipos.Items.Add(tipo.Type.Name);
+                    }
+
+                    // Cargar la imagen de forma asíncrona
+                    await CargarImagenAsync(pokemon.Sprites.FrontDefault);
+                }
+                else
+                {
+                    Console.WriteLine($"Error al cargar Pokémon. Código de estado: {response.StatusCode}");
+                    MessageBox.Show($"Error al cargar Pokémon. Código de estado: {response.StatusCode}");
+                }
+            }
+            catch (HttpRequestException ex)
+            {
+                Console.WriteLine("Error de red: " + ex.Message);
+                MessageBox.Show("Error de red: " + ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
+        private async Task CargarImagenAsync(string url)
+        {
+            try
+            {
+                using (var response = await client.GetAsync(url))
+                {
+                    response.EnsureSuccessStatusCode();
+                    var stream = await response.Content.ReadAsStreamAsync();
+
+                    if (stream.Length > 0) // Verificar que el stream no esté vacío
+                    {
+                        // Intentar cargar la imagen desde el stream
+                        using (var image = Image.FromStream(stream))
+                        {
+                            pbSprite.Image?.Dispose(); // Liberar la imagen anterior si existe
+                            pbSprite.Image = new Bitmap(image); // Asignar la nueva imagen
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("El stream de la imagen está vacío.");
+                        MessageBox.Show("No se pudo cargar la imagen del Pokémon.");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error al cargar la imagen: " + ex.Message);
+                MessageBox.Show("Error al cargar la imagen: " + ex.Message);
+            }
+        }
+
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            string nombrePokemon = textBox1.Text.Trim();
+            if (!string.IsNullOrEmpty(nombrePokemon))
+            {
+                await CargarPokemonAsync(nombrePokemon);
+            }
+            else
+            {
+                Console.WriteLine("Por favor, ingresa el nombre de un Pokémon.");
+                MessageBox.Show("Por favor, ingresa el nombre de un Pokémon.");
+            }
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lb2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
+
